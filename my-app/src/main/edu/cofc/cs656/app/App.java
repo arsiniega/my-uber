ddc7356b9809e62d990efa816ebbacd7377bdba0
Alan Arsiniega <arsiniega@gmail.com>
2020-11-19 15:54:00 -0500

    Creating new maven project my-app

---
+++ my-app/src/main/edu/cofc/cs656/app/App.java
@@ -0,0 +1,13 @@
+package edu.cofc.cs656.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
