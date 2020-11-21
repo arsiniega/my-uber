e37212d5dd96933c702ad63756bba2eee918910e
Alan Arsiniega <arsiniega@gmail.com>
2020-11-21 15:37:18 -0500

    correctly automated structure

---
+++ src/main/java/edu/cofc/cs656/uber/App.java
@@ -0,0 +1,13 @@
+package edu.cofc.cs656.uber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
